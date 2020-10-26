4db3c195f42c9fd21be6c9f4db323f2852f263c5
Volodymyr_Mushynskyi <volodya2127@gmail.com>
2020-10-26 09:22:09 +0200

    test jenkins job v4

--- src/test/java/test/EmailsTest.java
+++ src/test/java/test/EmailsTest.java
@@ -72,5 +72,6 @@ public class EmailsTest {
         SetEmailResponse response = (SetEmailResponse) client.callWebService(request);
         String codeResponse = response.getCode().getCodeStatus();
         assertEquals(codeResponse, "200");
+        System.out.println();
     }
 }
